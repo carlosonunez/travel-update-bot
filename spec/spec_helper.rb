8d7c989578c468ba8c3b2f766768558fef042eab
Carlos Nunez <carlosonunez@noreply.github.com>
2019-10-23 13:40:58 -0700

    Add spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,46 @@
+require 'rspec'
+require 'slack-api'
+require 'httparty'
+require 'aws-sdk-dynamodb'
 require 'capybara'
 require 'capybara/dsl'
 require 'selenium-webdriver'
 require 'vcr'
 require 'webmock'
+Dir.glob('/app/spec/helpers/**/*.rb') do |file|
+  require_relative file
+end
 
 VCR.configure do |vcr|
   vcr.cassette_library_dir = 'spec/cassettes'
   vcr.hook_info = :webmock
   vcr.configure_rspec_metadata!
+end
+
+RSpec.configure do |config|
+  config.include Capybara::DSL, :integration => true
+  config.before(:all, :integration => true) do
+    ['SELENIUM_HOST', 'SELENIUM_PORT'].each do |required_selenium_env_var|
+      raise "Please set #{required_selenium_env_var}" if ENV[required_selenium_env_var].nil?
+    end
+
+    $test_api_key =
+      Helpers::Integration::SharedSecrets.read_secret(secret_name: 'api_key') ||
+        raise('Please create the "api_key" secret.')
+
+    Capybara.run_server = false
+    Capybara.register_driver :selenium do |app|
+      Capybara::Selenium::Driver.new(
+        app,
+        browser: :remote,
+        url: "http://#{ENV['SELENIUM_HOST']}:#{ENV['SELENIUM_PORT']}/wd/hub",
+        desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+          "chromeOptions" => {
+            "args" => ['--no-default-browser-check']
+          }
+        )
+      )
+    end
+    Capybara.default_driver = :selenium
+  end
 end
