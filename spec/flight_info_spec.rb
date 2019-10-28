4ae8a8dbdc040c99564d6eb12344ccb800281c67
Carlos Nunez <carlosonunez@noreply.github.com>
2019-10-27 21:11:03 -0400

    Use epoch times

--- spec/flight_info_spec.rb
+++ spec/flight_info_spec.rb
@@ -3,19 +3,22 @@ require 'spec_helper'
 describe "Flight info" do
   context "When given a flight number" do
     it "Retrieves flight info", :unit, :vcr do
+      ENV['FLIGHTAWARE_URL'] = "file:///#{Dir.pwd}/spec/fixtures/test_flight_aa1.html"
       expected_flight_info_json = {
-        flight_number: "AAL1",
-        origin: "JFK",
-        destination: "LAX",
-        departure_time: "07:54 EDT",
-        est_takeoff_time: "08:18 EDT",
-        est_landing_time: "11:00 PDT",
-        arrival_time: "11:06 PDT"
-      }.to_json
-      expect(FlightInfo.get(flight_number: 'AA1')).to eq({
         statusCode: 200,
-        body: expected_flight_info_json
-      }.to_json)
+        body: {
+          flight_number: "AAL1",
+          origin: "JFK",
+          destination: "LAX",
+          departure_time: 1572177180,
+          est_takeoff_time: 1572178620,
+          est_landing_time: 1572199860,
+          arrival_time: 1572200280
+        }.to_json
+      }
+      actual_json = JSON.parse(FlightInfo.get(flight_number: 'AA1'),
+                               {symbolize_names: true})
+      expect(actual_json).to eq expected_flight_info_json
     end
   end
 end
