1e46da76eff01e511eb05bd061fba9330ed59e15
Carlos Nunez <carlosonunez@noreply.github.com>
2022-01-12 11:16:28 -0600

    ci: fixes

--- scripts/helpers/aws.sh
+++ scripts/helpers/aws.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 AWS_CREDS_TEMP_FP="$(mktemp ${TMPDIR:-/tmp}/aws-session-credentials-"$(date +%s)"-XXXXX)"
 generate_aws_credentials() {
+  info "Pulling AWS image to avoid next command mangling image pull info"
+  docker-compose -f docker-compose.deploy.yml pull obtain-aws-session-credentials
   info "Retrieving an AWS session token"
   printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
     $(docker-compose -f docker-compose.deploy.yml run -T --rm obtain-aws-session-credentials) \
